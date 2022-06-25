849c84a08192cd766289ee1de7b77cda944fb52d
Dennis  Kimani Kabutu <denoh6804@gmail.com>
2022-06-25 13:34:36 +0300

    Post handler added

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -43,6 +43,17 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Header','GET,POST,PUT,PATCH,DELETE,OPTIONS')
         return response
 
+    
+    @app.route('/questions', methods = ['POST'])
+    def post_route_handler():
+
+        body = request.get_json()
+        if ('searchTerm' in body):
+            return search_questions()
+        else:
+            return create_question()
+
+            
     #an endpoint to handle GET requests for all available categories.
     @app.route('/categories', methods=['GET'])
     def get_all_categories():
@@ -95,8 +106,8 @@ def create_app(test_config=None):
         # return Jsonified paginated list of questions, Total of number of Questions and Categories   
         return jsonify({
             "success":True,
-            "question":paginated_questions,
-            "number of total questions":len(Question.query.all()),
+            "questions":paginated_questions,
+            "total number of questions":len(Question.query.all()),
             "category":all_categories,
         })
     
@@ -104,14 +115,14 @@ def create_app(test_config=None):
 
     """
     Create an endpoint to DELETE question using a question ID.
-
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page.
     """
     @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
         try:
+            #Try querying the database to get the specified question
             question = Question.query.filter(Question.id==question_id).one_or_none()
+            #If question is None Throw a 404 error
+            # If found delete it and return success message and deleted question id 
             if question is None:
                 abort(404)
             else:
@@ -130,26 +141,30 @@ def create_app(test_config=None):
     which will require the question and answer text,
     category, and difficulty score.
     """
+
     
-    @app.route('/questions', methods = ['POST'])
+   
     def create_question():
+        #Get request Body using the get_json method.
         body = request.get_json()
+        #Load request params into variables
         new_question = body.get('question', None)
         new_answer = body.get('answer', None)
         new_category = body.get('category', None)
         new_difficulty = body.get('difficulty', None)
-
+        # If any request params is None throw a 400 error
         if(new_question is None or new_answer is None or new_category is None or new_difficulty is None):
                return abort(400)
 
         try:
+            #If all params are present create a Question
             question = Question(
                     question=new_question,
                     answer=new_answer,
                     category=new_category,
                     difficulty=new_difficulty
-                    )
-
+                )
+            # Insert the question to the database
             question.insert()
 
             return jsonify({
@@ -160,21 +175,12 @@ def create_app(test_config=None):
             return abort(422)
 
 
-
-
-           
-
-
     """
-    @TODO:
     Create a POST endpoint to get questions based on a search term.
     It should return any questions for whom the search term
     is a substring of the question.
-    TEST: Search by any phrase. The questions list will update to include
-    only question that include that string within their question.
-    Try using the word "title" to start.
     """
-    @app.route('/questions/search', methods=['POST'])
+
     def search_questions():
         try:
             body_data = request.get_json()
@@ -202,20 +208,18 @@ def create_app(test_config=None):
     """
   
     Create a GET endpoint to get questions based on category.
-
-    TEST: In the "List" tab / main screen, clicking on one of the
-    categories in the left column will cause only questions of that
-    category to be shown.
     """
     @app.route('/categories/<int:category_id>/questions' ,methods = ['GET'])
     def get_questions_by_category(category_id):
         try:
+            #Query for category based on category id.
             category = Category.query.filter_by(id=category_id).one_or_none()
-
+            #If category is None throw a 404 error
             if category is None:
                 abort(404)
-
+            #Query questions based on category.
             questions_based_categories = Question.query.filter_by(category = category.id).all()
+            #Format questions as a list.
             formatted_questions = [question.format() for question in questions_based_categories]
 
             return jsonify({
@@ -233,10 +237,6 @@ def create_app(test_config=None):
     This endpoint should take category and previous question parameters
     and return a random questions within the given category,
     if provided, and that is not one of the previous questions.
-
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not.
     """
     
     #create a route to quizes '/quizzes with a POST Method
